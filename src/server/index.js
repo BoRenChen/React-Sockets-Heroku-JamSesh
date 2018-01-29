fbcfb5ec10e6085c2fb35e82092472cb78ee29e4
seanHolahan <holahan.s@husky.neu.edu>
2018-01-29 14:45:02 -0500

    emit check

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,30 @@ app.get('/', (req, res, next) => {
 // sockets test
 io.on('connection', socket => {
   socket.emit('hello', { message: 'hello!!! from server!' })
+
+
+	client.on('join', function(data) {
+	     	console.log('joined the server from App.js - from server');
+
+	        client.emit('welcomeMsg', 'Connected to socket')
+	    });
+	    client.on('messages', function(data) {
+	           client.emit('broad', data);
+	           client.broadcast.emit('broad',data);
+	    });
+	    client.on('buttonPressed', function(data) {
+	        console.log("server pressed" + data);
+	        client.broadcast.emit('press', data);
+	    });
+	    client.on('buttonReleased', function(data){
+	        console.log("server released" + data);
+	      client.broadcast.emit('release', data);
+	    });
+	    client.on('disconnect', function () {
+	    console.log('Client disconnected...');
+	      client.emit('disconnected');
+	    });
+  
 });
 
 server.listen(port);
